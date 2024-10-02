6696ad7f0b73242db5e02f602793c863f2b0b738
Nahue <nlcostamagna@gmail.com>
2024-10-02 11:34:01 -0300

    ec2 message

--- app/ec2/app.py
+++ app/ec2/app.py
@@ -9,6 +9,7 @@ def start(event, context):
     print(f'Starting EC2 instance: {instance_id}')
     try:
         response = ec2_client.start_instances(InstanceIds=[instance_id])
+        print(f'Successfully started EC2 instance: {instance_id}')
         return {
             'statusCode': 200,
             'body': f'Successfully started EC2 instance: {instance_id}'
@@ -23,6 +24,7 @@ def stop(event, context):
     print(f'Stopping EC2 instance: {instance_id}')
     try:
         response = ec2_client.stop_instances(InstanceIds=[instance_id])
+        print(f'Successfully stoped EC2 instance: {instance_id}')
         return {
             'statusCode': 200,
             'body': f'Successfully stopped EC2 instance: {instance_id}'
